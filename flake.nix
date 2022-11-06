7bae79ebe3ad318c2a5f274d0a8793b1c3b1bf67
Andrew Jeffery <dev@jeffas.io>
2022-11-06 22:20:57 +0000

    Switch to crane for building

--- flake.nix
+++ flake.nix
@@ -5,48 +5,67 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    crane,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            overlays = [ rust-overlay.overlay ];
-            system = system;
-          };
-          rust = pkgs.rust-bin.stable.latest.default;
-          cargoNix = pkgs.callPackage ./Cargo.nix { };
-        in
-        rec
-        {
-          packages = {
-            exp = cargoNix.workspaceMembers.exp.build;
-          };
+    (
+      system: let
+        pkgs = import nixpkgs {
+          overlays = [rust-overlay.overlays.default];
+          inherit system;
+        };
+        rust = pkgs.rust-bin.stable.latest.default;
+        craneLib = crane.lib.${system};
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+        };
+        cargoArtifacts = craneLib.buildDepsOnly (
+          commonArgs
+          // {
+            pname = "exp-deps";
+          }
+        );
+        exp = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            cargoTestCommand = "echo skipping tests";
+          }
+        );
+      in rec
+      {
+        packages = {
+          inherit exp;
+          default = exp;
+        };
 
-          defaultPackage = packages.exp;
+        checks = {
+          inherit exp;
+        };
 
-          checks = {
-            exp = cargoNix.workspaceMembers.exp.build;
-          };
+        formatter = pkgs.alejandra;
 
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs;[
-              (rust.override {
-                extensions = [ "rust-src" "rustfmt" ];
-              })
-              cargo-edit
-              cargo-watch
-              cargo-udeps
-              cargo-insta
-              crate2nix
-              rust-analyzer
-              cargo-outdated
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust
+            cargo-watch
+            cargo-udeps
+            cargo-insta
+            rust-analyzer
+            cargo-outdated
 
-              rnix-lsp
-              nixpkgs-fmt
-            ];
-          };
-        }
-      );
+            rnix-lsp
+          ];
+        };
+      }
+    );
 }
