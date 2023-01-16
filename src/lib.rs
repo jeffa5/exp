7f577c350a3d19832000044177a25b0348e6d367
Andrew Jeffery <dev@jeffas.io>
2023-01-16 16:24:02 +0000

    Add convenience result type

--- src/lib.rs
+++ src/lib.rs
@@ -12,26 +12,28 @@ mod run;
 pub use analyse::{analyse, repeat_dirs, AnalyseConfig, AnalyseError};
 pub use run::{run, Environment, RunConfig, RunError};
 
+pub type ExpResult<T> = Result<T, Box<dyn Error>>;
+
 pub trait ExperimentConfiguration: Serialize + DeserializeOwned {
     /// Calculate the hash of the serialized version of this config.
-    fn hash(&self) -> Result<String, Box<dyn Error>> {
+    fn hash(&self) -> ExpResult<String> {
         let mut v = Vec::new();
         self.ser(&mut v)?;
         let config_hash = blake3::hash(&v).to_hex();
         Ok(config_hash.to_string())
     }
 
-    fn ser<W: std::io::Write>(&self, w: W) -> Result<(), Box<dyn Error>> {
+    fn ser<W: std::io::Write>(&self, w: W) -> ExpResult<()> {
         serde_json::to_writer(w, self)?;
         Ok(())
     }
 
-    fn ser_pretty<W: std::io::Write>(&self, w: W) -> Result<(), Box<dyn Error>> {
+    fn ser_pretty<W: std::io::Write>(&self, w: W) -> ExpResult<()> {
         serde_json::to_writer_pretty(w, self)?;
         Ok(())
     }
 
-    fn deser<R: std::io::Read>(r: R) -> Result<Self, Box<dyn Error>> {
+    fn deser<R: std::io::Read>(r: R) -> ExpResult<Self> {
         let conf = serde_json::from_reader(r)?;
         Ok(conf)
     }
@@ -43,14 +45,14 @@ pub trait Experiment {
 
     fn configurations(&mut self) -> Vec<Self::Configuration>;
 
-    async fn pre_run(&mut self, configuration: &Self::Configuration) -> Result<(), Box<dyn Error>>;
+    async fn pre_run(&mut self, configuration: &Self::Configuration) -> ExpResult<()>;
     async fn run(
         &mut self,
         configuration: &Self::Configuration,
         repeat_dir: &Path,
-    ) -> Result<(), Box<dyn Error>>;
+    ) -> ExpResult<()>;
     async fn post_run(&mut self, configuration: &Self::Configuration)
-        -> Result<(), Box<dyn Error>>;
+        -> ExpResult<()>;
 
     fn analyse(
         &mut self,
